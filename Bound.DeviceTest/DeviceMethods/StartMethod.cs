0245b7b33c73a991420d38cc99ad21ee1cb06314
Daniel Carlson Hörnlund <info@danielcarlson.net>
2022-05-12 19:06:32 +0200

    Base IoTHubDevice

---
+++ Bound.DeviceTest/DeviceMethods/StartMethod.cs
@@ -0,0 +1,38 @@
+﻿using Bound;
+using Device;
+using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using WorkoutData.Managers;
+
+namespace DeviceManager.Device.NewFolder
+{
+    public static class StartMethod
+    {
+ 
+        public static async Task<MethodResponse> OnStart(MethodRequest methodRequest, object userContext)
+        {
+            Program.IsRunning = true;
+            Program.DeviceData = JsonConvert.DeserializeObject<DeviceData>(methodRequest.DataAsJson);
+            Program.DeviceData.TrainingData = new List<TrainingData>();
+
+            while (Program.IsRunning)
+            {
+                Thread.Sleep(100);
+                var x = new Random().Next(0, 256);
+                var y = new Random().Next(0, 256);
+                var z = new DateTimeOffset(DateTime.UtcNow).ToUnixTimeSeconds();
+
+                Console.WriteLine($"{x}, {y}, {z}");
+
+                Program.DeviceData.TrainingData.Add(new TrainingData() { X = x, Y = y, Z = z });
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Bound.DeviceTest/DeviceMethods/StopMethod.cs
@@ -0,0 +1,45 @@
+﻿using Bound;
+using Device;
+using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using WorkoutData.Managers;
+
+namespace DeviceManager.Device.DeviceMethods
+{
+    public class StopMethod
+    {
+        static string BlobsConnectionString = "DefaultEndpointsProtocol=https;AccountName=boundalgorithmapi;AccountKey=hYgYq3IOQw+FSYxnl+1pXoYsSOlskLVLZQ11GUmDSmOBwzioLg4OvUL4P4pG/PQ7/lbRiNarZ1/42WfYEJ3AMQ==;EndpointSuffix=core.windows.net";
+        public static BlobsManager blobsManager = new BlobsManager(BlobsConnectionString);
+
+        public static async Task<MethodResponse> OnStop(MethodRequest methodRequest, object userContext)
+        {
+            Program.IsRunning = false;
+            var filePath = CreateBlobPath(Program.DeviceData);
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            string trainingData = JsonConvert.SerializeObject(Program.DeviceData.TrainingData);
+            _ = await blobsManager.AppendDataInBlob(filePath, trainingData);
+
+            Console.WriteLine(stopwatch.ElapsedMilliseconds + " millisekunder tog det att ladda upp datan");
+            Console.WriteLine("Lägnden på listan: " + Program.DeviceData.TrainingData.Count + " rader");
+            Console.WriteLine("Device stopped");
+
+            Program.DeviceData.TrainingData.Clear();
+
+            return null;
+        }
+        static string CreateBlobPath(DeviceData deviceData)
+        {
+            string blobPath = deviceData.ObjectId + "/";
+            blobPath += deviceData.MachineName.ToLower() + "/";
+            blobPath += DateTime.Now.ToString("yyyyMMdd") + ".txt";
+
+            return blobPath;
+        }
+    }
+}
